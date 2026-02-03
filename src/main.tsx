ccbdfe4fa655c0f0adcf5689071c09178be94eb0
athulthomasatz <thomasathul089@gmail.com>
2026-02-03 23:29:36 +0530

    Added hashrouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "./hooks/use-theme.tsx";
-import App from "./App.tsx";
+ { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // <--- Import this
+import App from './App.tsx'
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+createRoot(document.getElementById('root')!).render(
+  <HashRouter> 
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </HashRouter>,
+)
