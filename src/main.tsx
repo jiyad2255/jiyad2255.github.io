e822145c5728d1f59e97870f2eda5cb01018fdc9
athulthomasatz <thomasathul089@gmail.com>
2026-02-04 21:06:26 +0530

    Fixed the balnk page issue and added a new icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // <--- Import this
+import { HashRouter } from 'react-router-dom' 
 import { ThemeProvider } from "./hooks/use-theme.tsx";
 import App from './App.tsx'
 import "./index.css";
